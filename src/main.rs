3c28fc9e8ab2cbee1b127c930d7b8b5f426f8109
mazi <602281666@qq.com>
2021-11-26 14:08:47 +0800

    move 2

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 fn main() {
     let vec0 = Vec::new();
 
-    let mut vec1 = full_vec(vec0);
+    let mut vec1 = full_vec(vec0.clone());
 
-    println!("{} has length {} content `{:?}`", "vec1", vec1.len(), vec1);
+    println!("{} has length {} content `{:?}`", "vec0", vec0.len(), vec0);
 
     vec1.push(88);
 
